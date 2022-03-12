e15f1f32e1a54982c5379b90478ec46a128ae802
Sol <sodanabe@gmail.com>
2022-03-13 00:46:52 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import OperandForm from "./components/OperandForm";
+import {useState} from "react";
+
+
+function App() {
+
+    const [result, setResult] = useState(null);
+
+    function receiveResultFromServer(result) {
+        setResult(result);
+    }
+
+    return (
+        <div className="App">
+            <OperandForm url={new URL("http://localhost:8080/add")} receiveResult={receiveResultFromServer}/>
+            {result ? <div className="border p-3">Result is {result}</div> : ""}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/OperandForm.js
@@ -0,0 +1,55 @@
+import {useState} from "react";
+
+
+export default function OperandForm(props) {
+
+    const [left, setLeft] = useState(0);
+    const [right, setRight] = useState(0);
+
+    function handleLeftChanged(event) {
+        setLeft(parseInt(event.target.value));
+    }
+
+    function handleRightChanged(event) {
+        setRight(parseInt(event.target.value));
+    }
+
+    function handleResponse(response) {
+        if (!response.ok) {
+            throw new Error(`Status not OK: ${response.status} ${response.statusText}`);
+        }
+        return response.json();
+    }
+
+    function handleJson(jsonObj) {
+        props.receiveResult(jsonObj.result);
+    }
+
+    function handleError(error) {
+        alert(error.toString());
+    }
+
+    function handleFormSubmission(event) {
+        event.preventDefault();
+        props.url.search = new URLSearchParams([['left', left.toString()], ['right', right.toString()]]).toString();
+        fetch(props.url)
+            .then(handleResponse)
+            .then(handleJson)
+            .catch(handleError);
+    }
+
+    return (
+        <form className="border p-3" onSubmit={handleFormSubmission}>
+            <div className="mb-3 col">
+                <label htmlFor="leftInput" className="form-label">Left operand:</label>
+                <input type="number" className="form-control" id="leftInput" value={left} onChange={handleLeftChanged}/>
+            </div>
+            <div className="mb-3 col">
+                <label htmlFor="rightInput" className="form-label">Right operand:</label>
+                <input type="number" className="form-control" id="rightInput" value={right}
+                       onChange={handleRightChanged} />
+            </div>
+            <button type="submit" className="btn btn-primary">Compute</button>
+        </form>
+    );
+}
